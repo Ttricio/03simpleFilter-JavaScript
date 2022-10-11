9e472e798bc7f393adfce7a0f8d39148c8c72927
Patryk Kaczmarek <patrykmkaczmarek@gmail.com>
2022-10-11 11:09:22 +0200

    adding files

---
+++ script.js
@@ -0,0 +1,19 @@
+const search = document.querySelector(".search")
+const li = document.querySelectorAll("li")
+
+const searchEngine = (e) => {
+    const text = e.target.value.toLowerCase();
+    console.log(text);
+    li.forEach(el =>{
+        
+        if(el.textContent.toLocaleLowerCase().indexOf(text) !== -1){
+            el.style.display ='block'
+        }else{
+            el.style.display = 'none'
+        }
+    })
+
+    
+}
+
+search.addEventListener('keyup', searchEngine)
